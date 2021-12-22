99de3c65cb7ecaccfb8dd15641834afcf09c49bd
长路漫漫_唯剑作伴 <473991883@qq.com>
2021-12-22 14:23:50 +0800

    新增helloworld接口

---
+++ src/main/java/com/wcj/sa_wiki/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.wcj.sa_wiki.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author 翁丞健
+ * @Date 2021/12/22 14:12
+ * @Version 1.0.0
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
+}
