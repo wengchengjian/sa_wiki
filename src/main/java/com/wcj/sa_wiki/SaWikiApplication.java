6554c5508130516576182c71470e519ba4a668a9
长路漫漫_唯剑作伴 <473991883@qq.com>
2021-12-22 13:37:29 +0800

    增加logback配置文件

--- src/main/java/com/wcj/sa_wiki/SaWikiApplication.java
+++ src/main/java/com/wcj/sa_wiki/SaWikiApplication.java
@@ -1,13 +1,19 @@
 package com.wcj.sa_wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class SaWikiApplication {
+    private static final Logger log = LoggerFactory.getLogger(SaWikiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(SaWikiApplication.class, args);
+        Environment env =SpringApplication.run(SaWikiApplication.class, args).getEnvironment();
+        log.info("启动成功!");
+        log.info("项目地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
