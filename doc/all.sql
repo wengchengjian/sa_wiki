282d5236ac12ac0dd84e9d5db694d11a264ac901
长路漫漫_唯剑作伴 <473991883@qq.com>
2021-12-22 16:58:19 +0800

    新增ebook表，生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -5,3 +5,23 @@ create table `test` (
                         `name` varchar(50) not null default '' comment 'name',
                         `password` varchar(50) not null default '' comment 'password'
 )auto_increment=1;
+
+drop table if exists `ebook`;
+
+create table `ebook`(
+    `id` bigint primary key comment 'id',
+    `name` varchar(50) not null  comment '名称',
+    `category1_id` bigint comment '分类一',
+    `category2_id` bigint comment '分类二',
+    `cover` varchar(200) comment '封面',
+    `description` varchar(200) comment '描述',
+    `doc_count` int comment '文档数',
+    `view_count` int comment '阅读数',
+    `vote_count` int comment '点赞数'
+)engine=Innodb default charset=utf8mb4 comment '电子书';
+
+insert into `ebook`(id,name,description) values(1,'SpringBoot教程','SpringBoot教程');
+insert into `ebook`(id,name,description) values(2,'Vue教程','Vue教程');
+insert into `ebook`(id,name,description) values(3,'React教程','React教程');
+insert into `ebook`(id,name,description) values(4,'SSM教程','SSM教程');
+insert into `ebook`(id,name,description) values(5,'Mybatis教程','Mybatis教程');
