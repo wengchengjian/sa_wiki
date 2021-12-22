0d7c275cec6986526d6c457cd2acadfb183467d1
长路漫漫_唯剑作伴 <473991883@qq.com>
2021-12-22 13:10:01 +0800

    项目初始化

---
+++ src/main/java/com/wcj/sa_wiki/SaWikiApplication.java
@@ -0,0 +1,13 @@
+package com.wcj.sa_wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaWikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wcj/sa_wiki/SaWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcj.sa_wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
